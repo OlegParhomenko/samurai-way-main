386d71ee1bb8d63756bc65f615ee5bd2935e1bdc
Oleg Parhomenko <shocked194@gmail.com>
2024-12-10 17:38:49 +0300

    lessons 14 - css modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 // @flow 
 import * as React from 'react';
+import classes from './Header.module.css';
 
 type Props = {
     
 };
 export const Header = (props: Props) => {
     return (
-        <header className="header">
+        <header className={classes.header}>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGnmrgCWfgo_sMSRqHXnOKALHltLHZeny-4w&s"
                  alt=""/>
         </header>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,23 +1,24 @@
 // @flow 
 import * as React from 'react';
+import classes from  './Profile.module.css';
 
 type Props = {
     
 };
 export const Profile = (props: Props) => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <div>
                 <img
-                    src='https://image.cdn2.seaart.me/2023-08-25/15354882161664005/db6c025f8503a720e7944dc1e450fb446bbda97b_high.webp'/>
+                    src={'https://image.cdn2.seaart.me/2023-08-25/15354882161664005/db6c025f8503a720e7944dc1e450fb446bbda97b_high.webp'}/>
             </div>
             <div>ava + description</div>
             <div>my post
                 <div>New post</div>
             </div>
             <div>
-                <div>post 1</div>
-                <div>post 2</div>
+                <div className={classes.item}>post 1</div>
+                <div className={classes.item}>post 2</div>
             </div>
         </div>
     );
